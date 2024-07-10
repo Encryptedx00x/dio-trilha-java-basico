ac97444a0e44b2f1a9b1e11c4f8775d67c3c2aed
Encryptedx00x <dyogoho@gmail.com>
2024-07-09 23:51:21 -0300

    Estruturas condicionais

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -7,13 +7,13 @@ public class ContaTerminal {
         System.out.println("Por favor, digite seu nome: ");
         String nomeCliente = scanner.nextLine();
 
-        System.out.println("Por favor, digite o número da sua agência:");
+        System.out.println("Por favor, digite o número da sua agência: ");
         int agencia = scanner.nextInt();
 
-        System.out.println("Por favor, digite o número da conta:");
+        System.out.println("Por favor, digite o número da conta: ");
         String conta = scanner.next();
 
-        System.out.println("Por favor, digite o valor do saldo para saque:");
+        System.out.println("Por favor, digite o valor do saldo para saque: ");
         double numeroSaldo = scanner.nextDouble();
 
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo de R$ " + numeroSaldo + " já está disponível para saque.");
