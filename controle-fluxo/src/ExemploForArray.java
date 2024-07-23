a63dd6de1752ff123a5bdbaa632edaa8b92b3233
Encryptedx00x <dyogoho@gmail.com>
2024-07-23 18:48:43 -0300

    Estruturas de repetição

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,9 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        // Em arrays o índice inicia em zero.
+        String alunos [] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+        for(String aluno : alunos){
+            System.out.println("O nome do aluno é: " +aluno);
+        }
+    }
+}
